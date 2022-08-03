61bb3b544d1db1a22a0ebb5d0554b7c25a3d34a2
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2022-08-03 11:16:35 +0200

    add random string

--- main.tf
+++ main.tf
@@ -9,3 +9,9 @@ resource "random_password" "password2" {
   special          = true
   override_special = "_%@"
 }
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
