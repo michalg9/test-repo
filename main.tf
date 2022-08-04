3cd14a7ce5537177e89ace653f37331e9a163166
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2022-08-04 17:12:57 +0200

    another random string2

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ resource "random_string" "random" {
   special          = true
   override_special = "/@£$"
 }
+
+resource "random_string" "random2" {
+  length           = 12
+  special          = true
+  override_special = "/@£$"
+}
+
